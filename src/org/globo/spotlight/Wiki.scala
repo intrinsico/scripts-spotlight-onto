1f281c47fe938ab08fd518baf118e988acf9d739
Renan <ad.renan@gmail.com>
2013-11-21 18:12:14 -0200

    Fixes more encoding issue, fixes EGO page titles

--- src/org/globo/spotlight/Wiki.scala
+++ src/org/globo/spotlight/Wiki.scala
@@ -32,84 +32,85 @@ import java.net.URL
 import org.jsoup.Jsoup
 import org.jsoup.Connection.Method
 import java.nio.charset.Charset
-import org.mozilla.universalchardet.UniversalDetector
 
 object Wiki {
-  
-  val BEGIN_HTML = "DOCTYPE html"
-  val END_HTML = "</html>"
 
+  // Gets the that prevents encoding error according to the length of the string
+  def getCorrectString(utfString: String, isoString: String): String = {
+    if (isoString.length <= utfString.length) {
+      isoString
+    } else {
+      utfString
+    } 
+  }
+  
+  // Strips the title of unnecessary information according to the each origin
+  def getCorrectTitle(aTitle: String): String = {
+    aTitle match {
+      case t if t.contains("|") => aTitle.split("\\|")(0).dropRight(1)             
+      case t if t.contains("no G1") => aTitle.split("no G1")(0).dropRight(1)
+      case t if (t.startsWith("EGO ") && t.split(" - ").length >= 2) => aTitle.split(" - ")(1)
+      case t if t.contains("-") => aTitle.split(" - ")(0)
+      case _ => aTitle
+    }
+  }
+    
   def generateWiki(turtleFile: String, outputFile: String) {
     
+    val BEGIN_HTML = "DOCTYPE html"
+    val END_HTML = "</html>"
     val DEFAULT_ENCODING = "iso-8859-1"
     var buffer = new StringBuilder
     var pageBuffer = new StringBuilder    
 
-    buffer.append("""<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.6/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.6/ http://www.mediawiki.org/xml/export-0.6.xsd" version="0.6" xml:lang="pt">""" + "\n\t<siteinfo>\n\t\t<sitename>Globo</sitename>\n\t\t<base>http://semantica.globo.com</base>\n\t</siteinfo>\n")
-
-    val detector = new UniversalDetector(null)
+    buffer.append("""<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.6/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.6/ http://www.mediawiki.org/xml/export-0.6.xsd" version="0.6" xml:lang="pt">""" + "\n\t<siteinfo>\n\t\t<sitename>Globo</sitename>\n\t\t<base>http://semantica.globo.com</base>\n\t</siteinfo>\n")    
     
-    var i = 0
+    var i = 1
     for (line <- Source.fromFile(turtleFile).getLines()) {          
       
       try {
         if (!line.contains(END_HTML)) {	           
           pageBuffer.append(line)         
-        } else {
-          i += 1
-          if (i % 100 == 0) println (i + " HTML pages processed.")                   
-          val document = Jsoup.parse(pageBuffer.toString)         
-	      buffer.append("\t<page>\n")
-	      buffer.append("\t\t<title>")	
+        } else {          
+          if (i % 200 == 0) println (i + " HTML pages processed.")            
+          val document = Jsoup.parse(pageBuffer.toString)  
+          pageBuffer.delete(0, pageBuffer.length)
+	      pageBuffer = new StringBuilder
 	      
 	      // Get title according to case with UTF-8	      
-	      var encBytes = document.title().replaceAll("&","e").getBytes(DEFAULT_ENCODING)
-	      
-	      var title = new String(encBytes, "UTF-8")	          	      
-	      title match {
-            case t if t.contains("|") => buffer.append(title.split("\\|")(0).dropRight(1))             
-            case t if t.contains("no G1") => buffer.append(title.split("no G1")(0).dropRight(1))
-            case t if t.contains("-") => buffer.append(title.split(" - ")(0))
-            case _ => buffer.append(title)
-          }	      
-	      buffer.append("</title>\n")
-	      buffer.append("\t\t<ns>0</ns>\n")
-	      buffer.append("\t\t<id>" + i + "</id>\n")
-	      buffer.append("\t\t<revision>\n")
-	      buffer.append("\t\t\t<text>\n")
-	      buffer.append("\t\t\t")
+	      var encBytes = document.title().replaceAll("&","e").getBytes(DEFAULT_ENCODING)	      	                     
+	      val title = getCorrectTitle(getCorrectString(new String(encBytes, "UTF-8"), new String(encBytes, DEFAULT_ENCODING)))	                          	  	      	      	      	                   
 	      	      
-	      encBytes = document.body().text().replaceAll("&","e").getBytes("iso-8859-1")
-	      detector.handleData(encBytes, 0, encBytes.length)
-	      detector.dataEnd()
-	      
-	      val encoding = detector.getDetectedCharset()
-	      if (encoding == null) {
-	        encBytes = document.body().text().replaceAll("&","e").getBytes(DEFAULT_ENCODING)
-	      } else {
-	        encBytes = document.body().text().replaceAll("&","e").getBytes(encoding)
-	      }	       
-	      detector.reset()
-	      	      
-	      if (i < 17947) {
-	    	buffer.append(new String(encBytes, "UTF-8"))
-	      } else {
-	        buffer.append(new String(encBytes, "iso-8859-1"))
-	      }
-	      
-	      buffer.append("\n")
-	      buffer.append("\t\t\t</text>\n")
-	      buffer.append("\t\t</revision>\n")
-	      buffer.append("\t</page>\n")
-	      
-	      pageBuffer.delete(0, buffer.length)
-          pageBuffer = new StringBuilder
-          
-          if (i % 100 == 0 && !buffer.isEmpty) {
-	        appendToFile(outputFile, buffer.toString.dropRight(1))
-	        buffer.delete(0, buffer.length)
-	        buffer = new StringBuilder
-          }                   
+          // Skip pages without og:description property because this property defines the context
+	      encBytes = null
+	      encBytes = document.select("meta[property=og:description]").attr("content").replaceAll("&","e").getBytes(DEFAULT_ENCODING)
+	      if (encBytes.length != 0) {	      		   
+	        var context = getCorrectString(new String(encBytes, "UTF-8"), new String(encBytes, DEFAULT_ENCODING))		    		      	            	     	      	                   
+		      
+		    // The final part is to append the actual page to the buffer
+		    buffer.append("\t<page>\n")
+		    buffer.append("\t\t<title>")
+		    buffer.append(title)
+		    buffer.append("</title>\n")
+		    buffer.append("\t\t<ns>0</ns>\n")
+		    buffer.append("\t\t<id>" + i + "</id>\n")
+		    buffer.append("\t\t<revision>\n")
+		    buffer.append("\t\t\t<text>\n")
+		    buffer.append("\t\t\t")
+		    buffer.append(context)	      	    		     	      
+		    buffer.append("\n")
+		    buffer.append("\t\t\t</text>\n")
+		    buffer.append("\t\t</revision>\n")
+		    buffer.append("\t</page>\n")		      		  
+	          
+	        if (i % 200 == 0 && !buffer.isEmpty) {
+		      appendToFile(outputFile, buffer.toString.dropRight(1))
+		      buffer.delete(0, buffer.length)
+		      buffer = new StringBuilder
+	        }   
+	        i += 1	    
+	      }          
+          encBytes = null
 	    }    
       } catch {
         case e: IOException => println("An error occurred while parsing this page!")
